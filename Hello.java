b9a9214c012b3467475e1c7b92432cbb85e73b77
dlclxkzk87 <dlxkzk87@naver.com>
2025-04-02 10:26:33 +0900

    feat: 고객 관리

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello{
 	public static void main(String arg[]){
 		System.out.println("hello");
-		MemberDAO dao = new MemberDAO();
-		dao.insert();
+	//	MemberDAO dao = new MemberDAO();
+	//	dao.insert();
 	}
 }
